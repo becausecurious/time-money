134a3c7917a4d7b070cf623e7e9cd3fcb36361bd
becausecurious <80173504+becausecurious@users.noreply.github.com>
2022-02-03 12:55:17 +0100

    mostly works

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 console.log("Time = Money")
 
-// TODO: check why this breaks run code snippet on https://stackoverflow.com/questions/1480588/input-size-vs-width
-
-// TODO: replace sooner - https://stackoverflow.com/questions/19625502/chrome-extension-to-replace-text-in-web-pages-and-facebook-posts-before-text-i.
-
 const maybePluralize = (count, noun, suffix = 's') =>
     `${count} ${noun}${count !== 1 ? suffix : ''}`;
 
@@ -42,42 +38,120 @@ function convert(usdAmount, options) {
     return maybePluralize(round(years, 2), 'year');
 }
 
+var cached_options = null;
+
+function replacePrices(el, options) {
+    var n, a = [], walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
+    while (n = walk.nextNode()) {
+        forEveryNode(n);
+    }
+}
+
+get_options(function (options) {
+    console.log("loaded options")
+    cached_options = options;
+
+    if (document.readyState === 'complete') {
+        console.log("already complete");
+        replacePrices(document, options);
+    } else {
+        document.addEventListener("DOMContentLoaded", function () {
+            console.log("DOMContentLoaded");
+            // initial HTML document has been completely loaded and parsed
+            replacePrices(document, options);
+            //....
+        })
+        document.onreadystatechange = function () {
+            if (document.readyState === 'complete') {
+                console.log("readyState", document.readyState);
+                replacePrices(document, options);
+            }
+        }
+    }
+});
+
 function convertString(s, multiplier, options) {
     //If there are commas, get rid of them
     s = s.replace(/,/g, '');
     var result = convert(parseFloat(s) * multiplier, options);
 
-    console.log("Replace ", s, " -> ", result)
+    console.log("Replace ", s, " * ", multiplier, " -> ", result)
     return result;
 }
 
-function replacePrices(options) {
-
-    var pageText = document.body.innerHTML;
-
-    // TODO: handle cents https://www.walmart.com/
-    document.body.innerHTML = pageText.replace(/\$([\d,]+(?:\.\d+)?)/g,
+function replaceUsd(s, options) {
+    s = s.replace(/\$([\d,]+(?:\.\d+)?)/g,
         function (string, c1) {
             return convertString(c1, 1, options)
         });
 
-    var pageText = document.body.innerHTML;
-
-    document.body.innerHTML = pageText.replace(/([\d,]+(?:\.\d+)?)\s*¢/g,
+    s = s.replace(/([\d,]+(?:\.\d+)?)\s*¢/g,
         function (string, c1) {
             return convertString(c1, 0.01, options)
         });
+
+    return s;
 }
 
-get_options(function (options) {
-    if (document.readyState !== 'loading') {
-        replacePrices(options);
-    } else {
-        document.addEventListener("DOMContentLoaded", function () {
-            // initial HTML document has been completely loaded and parsed
-            replacePrices(options);
-            //....
-        });
-    }
+var textnode_backlog = [];
 
-});
+// https://stackoverflow.com/a/19630050
+// Modify the content somehow...
+var forEveryNode = function (node) {
+    var parent = null;
+    if (node.parentNode) {
+        parent = node.parentNode.nodeName.toLowerCase();
+    }
+    if (node.nodeName.toLowerCase() == "#text"
+        && parent != 'script'
+        && parent != 'style') {
+        if (cached_options) {
+            var new_value = replaceUsd(node.data, cached_options);
+            // Don't trigger mutation if we don't change the data.
+            if (node.data != new_value) {
+                node.data = new_value;
+            }
+        }
+    }
+}
+
+// TODO: handle replacing prices made of multiple text or even block elements.
+
+function observe(el) {
+    // Create a MutationObserver to handle events
+    // (e.g. filtering TextNode elements)
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type == 'characterData') {
+                if (mutation.oldValue != mutation.target.data) {
+                    forEveryNode(mutation.target);
+                }
+            }
+            if (mutation.addedNodes) {
+                [].slice.call(mutation.addedNodes).forEach(function (node) {
+                    forEveryNode(node);
+                });
+            }
+            if (mutation.type == 'childList') {
+                if (document.readyState === 'complete' && cached_options) {
+                    replacePrices(mutation.target, cached_options);
+
+                }
+            }
+        });
+    });
+
+    // Start observing "childList" events in document and its descendants
+    observer.observe(el, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        characterData: true,
+        characterDataOldValue: true,
+    });
+}
+
+observe(document);
+
+
+
